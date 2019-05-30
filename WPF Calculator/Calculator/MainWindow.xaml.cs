70bafc433c7ab40707872e77452f677242ee2b9a
Axyl <axyl@users.noreply.github.com>
2019-05-30 18:56:44 +0800

    30052019 1856

--- WPF Calculator/Calculator/MainWindow.xaml.cs
+++ WPF Calculator/Calculator/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,220 +21,52 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        char[] Operator = { '+', '-', '*', '/' };
         decimal num1 = 0, num2 = 0, answer = 0;
         string operation = "";
+        string lastNumber = "";
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Btn0_Click(object sender, RoutedEventArgs e)
+        private void Btn_Click(object sender, RoutedEventArgs e)
         {
-            int value = Convert.ToInt16((sender as Button).Content);
-
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + value;
-                txtDisplay.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + value;
-                txtDisplay.Text = num2.ToString();
-            }
+            char BtnValue = Convert.ToChar((sender as Button).Content);
+            Display(BtnValue);
         }
 
-        private void Btn1_Click(object sender, RoutedEventArgs e)
-        {
-            int value = Convert.ToInt16((sender as Button).Content);
+        //private void BtnPlus_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string value = (sender as Button).Content.ToString();
 
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + value;
-                txtDisplay.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + value;
-                txtDisplay.Text = num2.ToString();
-            }
-        }
+        //    txtDisplay.Text += value;
+        //}
 
-        private void Btn2_Click(object sender, RoutedEventArgs e)
-        {
-            int value = Convert.ToInt16((sender as Button).Content);
+        //private void BtnMinus_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string value = (sender as Button).Content.ToString();
+        //    txtDisplay.Text += value;
+        //}
 
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + value;
-                txtDisplay.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + value;
-                txtDisplay.Text = num2.ToString();
-            }
-        }
+        //private void BtnMultiply_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string value = (sender as Button).Content.ToString();
+        //    txtDisplay.Text += value;
+        //}
 
-        private void Btn3_Click(object sender, RoutedEventArgs e)
-        {
-            int value = Convert.ToInt16((sender as Button).Content);
+        //private void BtnDivide_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string value = (sender as Button).Content.ToString();
+        //    txtDisplay.Text += value;
+        //}
 
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + value;
-                txtDisplay.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + value;
-                txtDisplay.Text = num2.ToString();
-            }
-        }
-
-        private void Btn4_Click(object sender, RoutedEventArgs e)
-        {
-            int value = Convert.ToInt16((sender as Button).Content);
-
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + value;
-                txtDisplay.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + value;
-                txtDisplay.Text = num2.ToString();
-            }
-        }
-
-        private void Btn5_Click(object sender, RoutedEventArgs e)
-        {
-            int value = Convert.ToInt16((sender as Button).Content);
-
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + value;
-                txtDisplay.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + value;
-                txtDisplay.Text = num2.ToString();
-            }
-        }
-
-        private void Btn6_Click(object sender, RoutedEventArgs e)
-        {
-            int value = Convert.ToInt16((sender as Button).Content);
-
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + value;
-                txtDisplay.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + value;
-                txtDisplay.Text = num2.ToString();
-            }
-        }
-
-        private void Btn7_Click(object sender, RoutedEventArgs e)
-        {
-            int value = Convert.ToInt16((sender as Button).Content);
-
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + value;
-                txtDisplay.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + value;
-                txtDisplay.Text = num2.ToString();
-            }
-        }
-
-        private void Btn8_Click(object sender, RoutedEventArgs e)
-        {
-            int value = Convert.ToInt16((sender as Button).Content);
-
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + value;
-                txtDisplay.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + value;
-                txtDisplay.Text = num2.ToString();
-            }
-        }
-
-        private void Btn9_Click(object sender, RoutedEventArgs e)
-        {
-            int value = Convert.ToInt16((sender as Button).Content);
-
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + value;
-                txtDisplay.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + value;
-                txtDisplay.Text = num2.ToString();
-            }
-        }
-
-        private void BtnPlus_Click(object sender, RoutedEventArgs e)
-        {
-            string value = (sender as Button).Content.ToString();
-
-
-            Eval();
-
-            operation = value;
-        }
-
-        private void BtnMinus_Click(object sender, RoutedEventArgs e)
-        {
-            string value = (sender as Button).Content.ToString();
-
-
-            Eval();
-
-            operation = value;
-        }
-
-        private void BtnMultiply_Click(object sender, RoutedEventArgs e)
-        {
-            string value = (sender as Button).Content.ToString();
-
-
-            Eval();
-
-            operation = value;
-        }
-
-        private void BtnDivide_Click(object sender, RoutedEventArgs e)
-        {
-            string value = (sender as Button).Content.ToString();
-
-
-            Eval();
-
-            operation = value;
-        }
-
-        private void BtnDot_Click(object sender, RoutedEventArgs e)
-        {
-            string value = (sender as Button).Content.ToString();
-
-            txtDisplay.Text += value;
-        }
+        //private void BtnDot_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string value = (sender as Button).Content.ToString();
+        //    txtDisplay.Text += value;
+        //}
 
         private void BtnClear_Click(object sender, RoutedEventArgs e)
         {
@@ -320,6 +153,74 @@ namespace Calculator
             num2 = 0;
             operation = "";
             answer = 0;
+        }
+
+        private void Display(char BtnValue)
+        {
+            char LastValue = txtDisplay.Text[txtDisplay.Text.Length - 1];
+
+            switch (BtnValue)
+            {
+                case '0':
+
+                    if (string.IsNullOrEmpty(lastNumber))
+                    {
+                        txtDisplay.Text += LastValue != '0' && Operator.Contains(LastValue) ? BtnValue.ToString() : "";
+                    }
+                    else if(lastNumber[0] != '0')
+                    {
+                        txtDisplay.Text += BtnValue.ToString();
+                    }
+
+
+                    break;
+
+                case '.':
+
+                    if (!Operator.Contains(LastValue) && LastValue != '.' && !lastNumber.Contains('.'))
+                    {
+                        txtDisplay.Text += BtnValue.ToString();
+                    }
+                    else if (Operator.Contains(LastValue))
+                    {
+                        txtDisplay.Text += $"0{BtnValue.ToString()}";
+                    }
+
+
+                    break;
+
+                default:
+
+                    if (Char.IsDigit(BtnValue))
+                    {
+                        txtDisplay.Text = txtDisplay.Text == "0" ? BtnValue.ToString() : $"{txtDisplay.Text + BtnValue.ToString()}";
+                    }
+                    else
+                    {
+                        if (!Operator.Contains(LastValue) && LastValue != '.')
+                        {
+                            txtDisplay.Text += BtnValue.ToString();
+                        }
+                        else
+                        {
+                            txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1) + BtnValue.ToString();
+                        }
+
+
+                    }
+
+                    break;
+            }
+
+
+            if (!Operator.Contains(BtnValue))
+            {
+                lastNumber += BtnValue.ToString();
+            }
+            else if (Operator.Contains(BtnValue))
+            {
+                lastNumber = "";
+            }
         }
 
 
